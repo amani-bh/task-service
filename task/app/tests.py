55b028c5a6bf63ff0ca4cad45772e7d0a562bfa6
Amani Ben Hassine <amani.benhassine@esprit.tn>
2023-06-09 21:02:00 +0100

    Unit test

--- task/app/tests.py
+++ task/app/tests.py
@@ -1,3 +1,67 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
 
-# Create your tests here.
+from .models import Project, List, Item, Comment
+from .serializers import ProjectSerializer, ListSerializer, ItemSerializer, CommentSerializer
+
+class ProjectAPITestCase(APITestCase):
+    def setUp(self):
+        self.project = Project.objects.create(title='Test Project',owner=1)
+        self.list = List.objects.create(project=self.project, title='Test List')
+        self.item = Item.objects.create(list=self.list, title='Test Item')
+        self.comment = Comment.objects.create(item=self.item, body='Test Comment')
+
+    def test_add_project(self):
+        url = reverse('add_project')
+        data = {
+            'project': {
+                'title': 'New Project',
+                'description': 'Project description'
+            }
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Project.objects.count(), 2)
+
+    def test_projects_by_user(self):
+        url = reverse('projects_by_user', args=[self.project.owner])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['title'], self.project.title)
+
+    def test_project_by_id(self):
+        url = reverse('project_by_id', args=[self.project.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.project.title)
+
+    def test_add_list(self):
+        url = reverse('add_list')
+        data = {
+            'project': self.project.pk,
+            'title': 'New List'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(List.objects.count(), 2)
+
+    def test_add_item(self):
+        url = reverse('add_item')
+        data = {
+            'list': self.list.pk,
+            'title': 'New Item'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Item.objects.count(), 2)
+
+    def test_comments_by_item(self):
+        url = reverse('comments_by_item', args=[self.item.pk])
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['body'], self.comment.body)
+
+
--- task/app/urls.py
+++ task/app/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("add_project", views.add_project),
-    path("user_projects/<int:id>", views.projects_by_user),
-    path("project_by_id/<int:id>", views.project_by_id),
-    path("add_list", views.add_list),
-    path("add_item", views.add_item),
+    path("add_project", views.add_project, name='add_project'),
+    path("user_projects/<int:id>", views.projects_by_user, name='projects_by_user'),
+    path("project_by_id/<int:id>", views.project_by_id, name='project_by_id'),
+    path("add_list", views.add_list, name='add_list'),
+    path("add_item", views.add_item, name='add_item'),
     path("update_list_title/<int:id>", views.update_list_title),
     path("update_item_title/<int:id>", views.update_item_title),
     path("update_item_description/<int:id>", views.update_item_description),
     path("update_item_order/<int:id>", views.update_item_order),
     path("update_list_order/<int:id>", views.update_list_order),
-    path("comments_by_item/<int:id>", views.comments_by_item),
+    path("comments_by_item/<int:id>", views.comments_by_item, name='comments_by_item'),
     path("add_comment", views.add_comment),
     path("update_comment/<int:id>", views.update_comment),
     path("delete_comment/<int:id>", views.delete_comment),
